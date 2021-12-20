43d818f7b0734546b0996c4db44352b89402ec34
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2021-12-20 11:54:39 +1100

    added crit calculation based on lowest needed number to crit

--- src/D&D DPR Calculator.rb
+++ src/D&D DPR Calculator.rb
@@ -37,15 +37,68 @@ class DPR
     # The lowest number needed to crit
     @C=0
   end
+  def crit(crit)
+    # The lowest number needed to crit
+    @C=crit
+    # chance to crit
+    @P=0.0
+    # number range
+    @R=(0..20).to_a
+    @R[0] = nil
+    @R.each do |n|
+      next unless n
+      break if n > @C
+      if n < @C
+        @R[n] = nil
+      end
+    end
+    @R= @R.compact
+    @R= @R.length()
+    @P= @R/20.0
+    p @P
+  end
   def hit(dc, attack)
     # chance of succeding the attack/save
     @H = 0.0
+    # attack bonus
+    @A=0
+    # target AC
+    @M=0
+    # assign input values to attack bonus and target ac
     @M=dc
     @A=attack
-    @H = 1.0-((@M - @A)/20.0)
-    puts "#{@H}" 
+    # calculate chance to hit
+    @H =1.0-((@M-@A)/20.0)
+    puts " hit chance#{@H.round(2)}" 
   end
-  
+  def hit_adv(dc, attack)
+    # chance of succeding the attack/save
+    @H = 0.0
+    # attack bonus
+    @A=0
+    # target AC
+    @M=0
+    # assign input values to attack bonus and target ac
+    @M=dc
+    @A=attack
+    # calculate chance to hit
+    @H = 1.0-((@M - @A)/20.0)**2
+    puts " hit chance with advantage#{@H.round(2)}" 
+  end
+  def hit_dis(dc, attack)
+    # chance of succeding the attack/save
+    @H = 0.0
+    # attack bonus
+    @A=0
+    # target AC
+    @M=0
+    # assign input values to attack bonus and target ac
+    @M=dc
+    @A=attack
+    # calculate chance to hit
+    @H = ((20 - @M + @A)/20.0)**2
+    puts " hit chance with disadvantage#{@H.round(2)}" 
+  end
 end
 
 
@@ -60,6 +113,9 @@ if binanry == true
   print TTY::Box.frame "Brilliant, lets generate you a character profile right away. \nFor the following steps please refer to your D&D character who's \nDPR you would like to calculate. \n",  padding: 3, align: :center
 
   dpr = DPR.new
-  dpr.hit() 
-  
+  dpr.hit(16, 5) 
+  dpr.hit_adv(16, 5)
+  dpr.hit_dis(16, 5)
+  dpr.crit(20)  
+  dpr.crit(18)
 end
